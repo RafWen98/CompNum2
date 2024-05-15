0503533f06f2098e8b6b6d56de2887abcc92dbcf
RafWen98 <119746231+RafWen98@users.noreply.github.com>
2024-05-15 12:10:36 +0200

    no real change

--- 3_3_RelFehler.py
+++ 3_3_RelFehler.py
@@ -4,9 +4,6 @@ import matplotlib.pyplot as plt     #for plots
 import utils                        #utils.py file containg functions defined by us
 import qol
 
-def relative_error(approx, exact):
-    return 
-
 def n_needed_for_accuracy(x, accuracy = 100*10e-16):
     """calulates the needed n for a desired accuracy of sin(x)"""
     VF = 100; n = 0
@@ -17,14 +14,13 @@ def n_needed_for_accuracy(x, accuracy = 100*10e-16):
     return n, VF
 
 def calculate_errors():
-    k_max = 10
+    k_max = 8
     x_values = [2 ** k for k in range(k_max + 1)] + [710]
-    reference_values = [math.sin(x) for x in x_values]
 
-    for i, x in enumerate(x_values):
+    for x in x_values:
         n, VF = n_needed_for_accuracy(x)
-        approx_value = utils.sinTaylor(x, n)  # Using n=10 as an example
-        exact_value = reference_values[i]
+        approx_value = utils.sinTaylor(x, n)
+        exact_value = math.sin(x)
         rel_error = abs(approx_value - exact_value) / abs(exact_value)
         print(f"Argument: {x}, n used: {n}, Relative Error: {rel_error}")
 
